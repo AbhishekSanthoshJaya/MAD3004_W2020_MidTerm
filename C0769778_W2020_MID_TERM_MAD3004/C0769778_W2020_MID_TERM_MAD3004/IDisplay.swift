9108ec1c8244f69f73f52d0730c79a39b1fece98
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-13 17:55:20 -0500

    [Abhishek] Added IDisplay protocol

---
+++ C0769778_W2020_MID_TERM_MAD3004/C0769778_W2020_MID_TERM_MAD3004/IDisplay.swift
@@ -0,0 +1,16 @@
+//
+//  IDisplay.swift
+//  C0769778_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+//protocol containing the display method
+
+protocol IDisplayDelegate
+{
+     func display();
+}
