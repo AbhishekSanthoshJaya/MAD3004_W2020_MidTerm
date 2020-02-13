c396f2bc5a8ee3740ccfaf108c5d015df5dfcab4
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-13 18:01:46 -0500

    Added Hydro Class

---
+++ C0769778_W2020_MID_TERM_MAD3004/C0769778_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,23 @@
+//
+//  Hydro.swift
+//  C0769778_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+public class Hydro: Bill
+  {
+  private var agencyName: String
+  private var unitsUsed: Int
+
+init(billId: String, billDate: Date, billType: BillType, agencyName:String, unitsUsed: Int)
+  {
+    super.init(billId: billId, billDate: billDate, billType: billType)
+    self.agencyName = agencyName
+    self.unitsUsed = unitsUsed
+        
+  }
+}
