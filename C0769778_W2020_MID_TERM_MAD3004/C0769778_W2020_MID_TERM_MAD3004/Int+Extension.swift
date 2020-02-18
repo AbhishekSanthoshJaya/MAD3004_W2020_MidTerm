b23499096d7969534962bbd05810ecc449f41e25
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-18 15:51:57 -0500

    [Abhishek]Added Int extension

---
+++ C0769778_W2020_MID_TERM_MAD3004/C0769778_W2020_MID_TERM_MAD3004/Int+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Int+Extension.swift
+//  C0769778_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-18.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+//INT EXTENSION START
+extension Int
+{
+func addMobGB() -> String
+  {
+  return "\(self) GB"
+  }
+
+  func addMins() -> String
+  {
+  return "\(self) MINS"
+  }
+}
+//INT EXTENSION END
